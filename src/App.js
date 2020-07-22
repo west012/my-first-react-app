fe1931df13fc8032a6dfd48bec578bc496962727
west012 <soul98120@gmail.com>
2020-07-22 12:29:01 -0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src='https://64.media.tumblr.com/b327e0a64014ac4f2bbfa82afc036853/tumblr_mqh3s5yZAN1sucxz9o1_500.gif' />
+        <h1>
+          Welcome to my first React App!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
